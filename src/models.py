a66fdb0fe1f6f53cadc3b1cce05953c7e652e86c
borisbrunop <borisbruno88@gmail.com>
2020-09-24 21:58:44 +0000

    correccion de conflictos en serializacion del id

--- src/models.py
+++ src/models.py
@@ -58,11 +58,7 @@ class User(db.Model):
 
     def serialize(self):
         return {
-<<<<<<< HEAD
-            
-=======
             'id' : self.id,
->>>>>>> 01d37150d8bbe7c4d28c1b6e70ae9a73c0ef27b4
             'email' : self.email,
             'name' : self.name,
             'last_name' : self.last_name,
